f5d17c932cd956d3b488ae1ae32df3d9610c8424
ilyan ditama <ilyan.ditama@gmail.com>
2026-01-19 12:48:23 +0700

    initial commit for backend project - setup springboot - add gradle wrapper and initial docker compose

---
+++ backend/src/main/java/com/book/collection/book_collection_manager/BookCollectionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.book.collection.book_collection_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCollectionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCollectionManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/book/collection/book_collection_manager/BookCollectionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.collection.book_collection_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCollectionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
